37e49b1a5ff3119db2077952602998d6d8e3c16a
Hridoy-ii <nmohammad.hriidoy@gmail.com>
2023-05-23 01:15:45 +0600

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5600;
 const categories = require('./data/categories.json');
 
 app.use(cors());
